b83d35d685fe6fc5984e6e84fa7706b82a9694c7
aangLi <517249177@qq.com>
2019-10-23 23:23:50 +0800

    2019-10-23 对于home界面的设计和资料的收集

---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-container">
+    <div class="home-background">
+      <div class="home-image"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
